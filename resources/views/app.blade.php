c7602150dcc9351551b75069831dfe497bc8bb63
kawsar <rjkawsar151@gmail.com>
2026-07-20 13:40:06 +0600

    feat: implement admin authentication system and add Google Analytics tracking across views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16889620416"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-16889620416');
+    </script>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Best Salon in Gulshan | Adonis Men's Grooming Salon Dhaka</title>
